7a398c7850c609421713357cd78bc597fd05bd95
Ethan Zou <eazy975@gmail.com>
2017-01-18 11:25:20 -0500

    added validation for users, deleted superAdmins file

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 const User = require('../models/schemas/user');
+const validator = require('email-validator');
 
-
+exports.getAllUsers = (req, res, next) => {
+    User.find({}, (err, users) => {
+        if (err) return next(err);
+        res.json(users);
+    })
+}
 exports.getUserById = (req, res, next) => {
-    if (req.params.id !== req.user.id && !req.user.isAdmin)
+    if (req.params.id !== req.user.id && !req.user.isSuperAdmin)
         return res.status(403).send("You don't have permission to do that");
     User.findById(req.params.id, (err, user) => {
         if (err) return next(err);
@@ -12,26 +18,73 @@ exports.getUserById = (req, res, next) => {
     });
 };
 
-// TODO verification if inputs are good.
 exports.createUser = (req, res, next) => {
-    if (req.body.password) req.body.hash = req.body.password;
-    var newUser = new User(req.body);
-    newUser.save()
-    .then((result) => next())
-    .catch((err) => {
-        if (err.code === 11000)
-            return res.status(400).send('Email already registered');
-        next(err)
+    if (typeof req.body.phone !== 'string')
+        return res.status(400).send('No phone');
+    if (typeof req.body.phoneProvider !== 'string')
+        return res.status(400).send('No phoneProvider');
+
+    var userData = {};
+
+    if (req.body.firstName && typeof req.body.firstName === 'string')
+        userData.firstName = req.body.firstName;
+    if (req.body.lastName && typeof req.body.lastName === 'string')
+        userData.lastName = req.body.lastName;
+
+    if (req.body.phoneProvider === 'other') {
+        if (typeof req.body['other-provider'] !== 'string')
+            return res.status(400).send('Missing other-provider');
+        userData.phoneProvider = req.body['other-provider'];
+    } else userData.phoneProvider = req.body.phoneProvider;
+
+    //validate phone (just needs 10 digits)
+    var phone = '';
+    for (var i = 0; i < req.body.phone.length; i++) {
+        if (!isNaN(req.body.phone[i]) && req.body.phone[i] !== ' ')
+            phone += req.body.phone[i];
+    }
+    if (phone.length !== 10)
+        return res.status(400).send('Invalid phone');
+    userData.phone = phone;
+
+    //validate email
+    if (req.body.email) {
+        if (!validator.validate(req.body.email))
+            return res.status(400).send('Invalid email');
+        else
+            userData.email = req.body.email;
+    }
+
+    if (req.body.password) 
+        req.body.hash = req.body.password;
+
+    var newUser = new User(userData);
+    newUser.save((err, user) => {
+        if (err) {
+            if (err.code === 11000)
+                return res.status(400).send('Email or phone number already registered');
+            return next(err);
+        }
+        return res.sendStatus(200);
     });
 };
 
 // TODO verification if inputs are good
 exports.updateUser = (req, res, next) => {
-    if (req.params.id !== req.user.id && !req.user.isAdmin)
+    if (req.params.id !== req.user.id && !req.user.isSuperAdmin)
         return res.status(403).send("You don't have permission to do that");
     User.findByIdAndUpdate(req.user.id, req.body, (err, doc) => {
         if (err) return next(err);
         if (!doc) return res.status(404).send('No user with that ID');
+        res.sendStatus(200);
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+    if (req.params.id !== req.user.id && !req.user.isSuperAdmin)
+        return res.status(403).send("You don't have permission to do that");
+    User.findByIdAndRemove(req.params.id, (err) => {
+        if (err) return next(err);
         res.sendStatus(200);
     });
 };
