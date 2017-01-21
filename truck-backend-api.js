e74d862f3ce329fba88356c6490859fd73c21e56
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-21 03:58:23 -0500

    Use bodyParser.urlencoded() extended flag
    Allows for subobjects in the req.body object:
    https://github.com/expressjs/body-parser#extended

--- truck-backend-api.js
+++ truck-backend-api.js
@@ -23,7 +23,7 @@ if (app.get('env') !== 'production') app.use(logger('dev'));
 require('./init/init');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ==================================================
